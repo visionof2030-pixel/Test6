86ed7344e77cd0221cb43fcbc579f4cb7aa48196
visionof2030-pixel <visionof2030@gmail.com>
2026-06-25 02:13:36 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+const HOST = "http://amir11.bounceme.net:8080";
+const USER = "YZjwcwnLjh";
+const PASS = "QlXrgJTnED";
+
+app.get("/*", async (req, res) => {
+  const url = `${HOST}${req.originalUrl}`;
+
+  const finalUrl = url.includes("?")
+    ? `${url}&username=${USER}&password=${PASS}`
+    : `${url}?username=${USER}&password=${PASS}`;
+
+  try {
+    const response = await fetch(finalUrl);
+    const data = await response.text();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(data);
+  } catch (err) {
+    res.status(500).send("error");
+  }
+});
+
+app.listen(3000);
